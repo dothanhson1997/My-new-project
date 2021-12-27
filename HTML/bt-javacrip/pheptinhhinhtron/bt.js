de85b5f31f72b9dd6539cc9d70d3cbf549fff3ca
dothanhson1997 <96030265+dothanhson1997@users.noreply.github.com>
2021-12-27 17:41:40 +0700

    bt-js

--- HTML/bt-javacrip/pheptinhhinhtron/bt.js
+++ HTML/bt-javacrip/pheptinhhinhtron/bt.js
@@ -1,6 +1,5 @@
-let Pi = 3.14;
-let R = Number(prompt("Nhập R: "));
-let C = 2 * R * Pi;
-let S = R*R * Pi;
-alert("Chu vi hình tròn là: " + C);
-alert("Diện tích hình tròn là: " + S);
+const Pi = 3.14;
+let r = prompt("Nhập R: ");
+let C = 2 * r * Pi;
+let S = r * r * Pi;
+document.write("Chu vi hình tròn là: " + C + "</br>" + "Diện tích hình tròn là: " + S)
--- HTML/bt-javacrip/tinhlainganhang/laisuatNH.js
+++ HTML/bt-javacrip/tinhlainganhang/laisuatNH.js
@@ -1,7 +1,7 @@
-let Money =Number(prompt("Nhập số tiền gửi: "));
+let Money =Number(prompt("Nhập số tiền vay: "));
 let interestrate = Number(prompt("Nhập % lãi suất: "));
-let n =Number(prompt("Nhập năm: "));
-let interest = Money * interestrate;
+let n =Number(prompt("Nhập số năm: "));
+let interest = Money * interestrate/100;
 let PAI = Money + n * interest;
-alert("Sau "+ n + " năm số tiền cả gốc lẫn lãi là: " + PAI)
+alert("Sau "+ n + " năm số tiền cả gốc lẫn lãi là: " + PAI + " VNĐ")
 
