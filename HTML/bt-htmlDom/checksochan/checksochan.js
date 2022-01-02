61170f055deaa6de0743feabe5c32573a00674b7
dothanhson1997 <96030265+dothanhson1997@users.noreply.github.com>
2022-01-02 22:36:08 +0700

    bt_htmldom

---
+++ HTML/bt-htmlDom/checksochan/checksochan.js
@@ -0,0 +1,12 @@
+function kt()
+{
+    let number = document.getElementById("num").value;
+    if(number % 2 == 0)
+    {
+        alert(number + " là số chẵn");
+    }
+    else
+    {
+        alert(number + " là số lẻ");
+    }
+}
---
+++ HTML/bt-javacrip/tankpro/tankpro.js
@@ -0,0 +1,69 @@
+let tank = document.getElementById("tank");
+let boom = document.getElementById("boom");
+let top_boom = parseInt(boom.style["top"]);
+let left_boom = parseInt(boom.style["left"]);
+
+function moveUp()
+{
+    let top_tank = parseInt(tank.style["top"]);
+    let left_tank = parseInt(tank.style["left"]);
+    tank.style["top"] = top_tank - 19 + "px";
+
+    top_tank = parseInt(tank.style["top"]);
+    left_tank = parseInt(tank.style["left"]);
+
+    if(top_tank == top_boom && left_tank == left_boom)
+    {
+        tank.style.backgroundImage = "url('https://media.istockphoto.com/vectors/boom-effect-vector-id916028398')"
+        tank.style.zIndex = 1;
+    }
+}
+
+
+function moveDown()
+{
+    let top_tank = parseInt(tank.style["top"]);
+    let left_tank = parseInt(tank.style["left"]);
+    tank.style["top"] = top_tank + 19 + "px";
+
+    top_tank = parseInt(tank.style["top"]);
+    left_tank = parseInt(tank.style["left"]);
+
+    if(top_tank == top_boom && left_tank == left_boom)
+    {
+        tank.style.backgroundImage = "url('https://media.istockphoto.com/vectors/boom-effect-vector-id916028398')"
+        tank.style.zIndex = 1;
+    }
+}
+
+function moveRight()
+{
+    let top_tank = parseInt(tank.style["top"]);
+    let left_tank = parseInt(tank.style["left"]);
+    tank.style["left"] = left_tank + 19 + "px";
+
+    top_tank = parseInt(tank.style["top"]);
+    left_tank = parseInt(tank.style["left"]);
+
+    if(top_tank == top_boom && left_tank == left_boom)
+    {
+        tank.style.backgroundImage = "url('https://media.istockphoto.com/vectors/boom-effect-vector-id916028398')"
+        tank.style.zIndex = 1;
+    }
+}
+
+function moveLeft()
+{
+    let top_tank = parseInt(tank.style["top"]);
+    let left_tank = parseInt(tank.style["left"]);
+    tank.style["left"] = left_tank - 19 + "px";
+
+    top_tank = parseInt(tank.style["top"]);
+    left_tank = parseInt(tank.style["left"]);
+
+    if(top_tank == top_boom && left_tank == left_boom)
+    {
+        tank.style.backgroundImage = "url('https://media.istockphoto.com/vectors/boom-effect-vector-id916028398')"
+        tank.style.zIndex = 1;
+    }
+}
