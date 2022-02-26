7982485c725796177bfc5e35079594e5b91bd049
dothanhson1997 <96030265+dothanhson1997@users.noreply.github.com>
2022-02-26 13:10:19 +0700

    case study

--- HTML/Case/Case.js
+++ HTML/Case/Case.js
@@ -19,16 +19,40 @@ function Add() {
     array_birthday.push(birthday);
     array_email.push(email);
     array_phone.push(phone);
+    if (name == '') {
+        alert("Họ và tên không được để trống!!");
+    } else if (sex == '') {
+        alert("Giới tính gì ông nội");
+    } else if (address == '') {
+        alert("Chưa nhập địa chỉ kìa cha!!");
+    } else if (birthday == '') {
+        alert("Nhập sinh nhật đi chứ");
+    } else if (email == '') {
+        alert("Cho xin Email luôn đi");
+    } else if (phone == '') {
+        alert("Lưu số điện thoại vào nữa");
+    }
 }
 
 function Dp() {
-    let list = "<tr>";
+    let list = "<th/>";
     for (let i = 0; i < array_name.length; i++) {
-        list += "Tên sinh viên:  " + array_name[i] + "<br>" + "Giới tính: " + array_sex[i] + "<br>" + "Sinh ngày: " + array_birthday[i] + "<br>" + "Địa chỉ: " + array_address[i] + "<br>" + "Email: " + array_email[i] + "<br>" + "Điện thoại: " + array_phone[i] + "<br/>" + "<br/>";
+        list += "STT " + i + "<br>" + " Tên sinh viên:  " + array_name[i] + "<br>" + "Giới tính: " + array_sex[i] + "<br>" + "Sinh ngày: " + array_birthday[i] + "<br>" + "Địa chỉ: " + array_address[i] + "<br>" + "Email: " + array_email[i] + "<br>" + "Điện thoại: " + array_phone[i] + "<br/>" + "<br/>";
     }
     document.getElementById("result").innerHTML = list;
 }
 
 function Del() {
     array_name.pop();
+}
+
+function Find(value) {
+    value = prompt("Nhập tên sinh viên");
+    for (let i = 0; i < array_name.length; i++) {
+        if (value == array_name[i]) {
+            alert("Sinh viên " + array_name[i] + " nằm ở số thứ tự là " + i);
+        } else(value == ''); {
+            alert("Không tìm thấy sinh viên này");
+        }
+    }
 }
